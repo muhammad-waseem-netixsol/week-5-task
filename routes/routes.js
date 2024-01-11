5e6b13bffed8cddf642ae4b920088b5ad7150e5f
muhammad-waseem-netixsol <muhammad_waseem_intern@netixsol.com>
2024-01-12 03:17:30 +0500

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,10 @@ const jwt_auth_check = require("../auth-check-mw/auth");
 
 router.get("/products",jwt_auth_check ,controllers.getAllProducts);
 
-router.post("/product", jwt_auth_check, upload.single('image') ,controllers.addNewProduct);
+router.post("/product", jwt_auth_check, upload.single('image'),[ 
+    body('title').notEmpty().withMessage('Title is required'),
+    body('price').notEmpty().withMessage('Price is required').isNumeric().withMessage('Price should be a number'),
+    ],controllers.addNewProduct);
 
 router.put("/product",jwt_auth_check,[ 
 body('title').notEmpty().withMessage('Title is required'),
